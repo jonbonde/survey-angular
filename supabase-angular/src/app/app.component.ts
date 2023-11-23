555c19e2a07b7979fe512dd2de7c2ff130ebfad7
Jonas Dankertsen <jhdank@gmail.com>
2023-11-23 15:29:00 +0100

    Litt pynt

--- supabase-angular/src/app/app.component.ts
+++ supabase-angular/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SupabaseService } from './supabase.service';
 import { Router } from '@angular/router';
+import { Survey } from './Survey';
+import { QuestionsService } from './questions.service';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +11,11 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'supabase-angular';
+  surveys!: Survey[];
 
   session = this.supabase.session;
 
-  constructor(private readonly supabase: SupabaseService, private router: Router) {  }
+  constructor(private readonly supabase: SupabaseService, private router: Router, private questionsService: QuestionsService) {  }
 
   ngOnInit() {
     this.supabase.authChanges((_, session) => (this.session = session));
@@ -20,5 +23,9 @@ export class AppComponent {
     if (this.session?.user.email == '') {
       this.router.navigate(['/Login']);
     }
+
+    this.questionsService.getSurveys().subscribe((data) => {
+      this.surveys = data;
+    });
   }
 }
